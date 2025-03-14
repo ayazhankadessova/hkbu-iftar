2fed4dbdf53cbf0e019519f610927ee5c7ef942a
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-03-14 14:38:49 +0800

    fixed colors

--- components/hkbu-iftar.tsx
+++ components/hkbu-iftar.tsx
@@ -3,14 +3,14 @@ import { Button } from '@/components/ui/button'
 // import { Card, CardContent } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
-import {
-  MoonStar,
-  //   CalendarDays,
-  //   UtensilsCrossed,
-  //   GiftIcon,
-  //   Image,
-  LocateIcon,
-} from 'lucide-react'
+// import {
+//   MoonStar,
+//   //   CalendarDays,
+//   //   UtensilsCrossed,
+//   //   GiftIcon,
+//   //   Image,
+//   LocateIcon,
+// } from 'lucide-react'
 import {
   Accordion,
   AccordionContent,
@@ -28,14 +28,14 @@ interface ScheduleItemProps {
 
 function ScheduleItem({ icon, title, time }: ScheduleItemProps) {
   return (
-    <div className='w-full flex items-center gap-4 py-5 border-b border-red-400/30'>
+    <div className='w-full flex items-center gap-4 py-5 border-b border-rose-400/30'>
       <div className='w-14 h-14 bg-black/5 rounded-[30px] flex items-center justify-center text-4xl'>
         {icon}
       </div>
       <div className='flex-1'>
-        <h3 className='text-green-800 text-xl font-medium'>{title}</h3>
+        <h3 className='text-green-950 text-xl font-medium'>{title}</h3>
       </div>
-      <div className='text-green-800 text-xl'>{time}</div>
+      <div className='text-green-950 text-xl'>{time}</div>
     </div>
   )
 }
@@ -47,9 +47,9 @@ interface ActivityItemProps {
   time: string
 }
 
-function ActivityItem({ title, description, time }: ActivityItemProps) {
+function ActivityItem({ title, description }: ActivityItemProps) {
   return (
-    <div className='w-full flex items-center gap-4 py-5 border-b border-red-400/30'>
+    <div className='w-full flex items-center gap-4 py-5 border-b border-rose-400/30'>
       <div className='w-24 h-24 bg-zinc-300/50 rounded-md flex items-center justify-center'>
         <Image
           src='https://picsum.photos/200'
@@ -60,7 +60,7 @@ function ActivityItem({ title, description, time }: ActivityItemProps) {
         />
       </div>
       <div className='flex-1 space-y-2'>
-        <h3 className='text-green-800 text-xl font-medium'>{title} ↗</h3>
+        <h3 className='text-green-950 text-xl font-medium'>{title} ↗</h3>
         <p className='text-stone-500'>{description}</p>
       </div>
     </div>
@@ -72,67 +72,71 @@ export function HKBUIftar() {
     <div className='min-h-screen flex flex-col'>
       {/* Header/Navigation */}
       {/* Header/Navigation */}
-      <header className='w-full px-8 md:px-20 lg:px-40 h-20 bg-white shadow-md shadow-red-400/10 flex justify-between items-center fixed top-0 z-50'>
+      <header className='w-full px-8 md:px-20 lg:px-40 h-20 bg-white border-b border-rose-400/30 flex justify-between items-center fixed top-0 z-50'>
         {/* MoonStar on left */}
         <div className='flex items-center'>
-          <p className='text-yellow-600'>HKBU Iftar</p>
+          <Image src='/logo.png' alt='Logo' width={85} height={50} />
         </div>
 
         {/* Navigation links on right */}
-        <nav className='flex justify-center items-center gap-8'>
+        {/* <nav className='flex justify-center items-center gap-8'>
           <a
             href='#about'
-            className='text-green-800 text-base font-normal hover:text-green-800 transition-colors'
+            className='text-green-950 text-base font-normal hover:text-green-950 transition-colors'
           >
             About
           </a>
           <a
             href='#schedule'
-            className='text-green-800 text-base font-normal hover:text-green-800 transition-colors'
+            className='text-green-950 text-base font-normal hover:text-green-950 transition-colors'
           >
             Schedule
           </a>
           <a
             href='#activities'
-            className='text-green-800 text-base font-normal hover:text-green-800 transition-colors'
+            className='text-green-950 text-base font-normal hover:text-green-950 transition-colors'
           >
             Activities
           </a>
           <a
             href='#faq'
-            className='text-green-800 text-base font-normal hover:text-green-800 transition-colors'
+            className='text-green-950 text-base font-normal hover:text-green-950 transition-colors'
           >
             FAQ
           </a>
           <a
             href='#feedback'
-            className='text-green-800 text-base font-normal hover:text-green-800 transition-colors'
+            className='text-green-950 text-base font-normal hover:text-green-950 transition-colors'
           >
             Feedback
           </a>
-        </nav>
+        </nav> */}
       </header>
 
       {/* Hero Section */}
       {/* Hero Section */}
       <section className='w-full px-8 md:px-20 lg:px-40 py-14 bg-[url(/green-bg.png)] bg-cover bg-center flex flex-col md:flex-row justify-center items-center gap-8 mt-20'>
-        <div className='w-full md:w-1/2 rounded-md flex justify-center md:justify-start items-center'>
+        <div className='w-3/4 md:w-1/2 flex justify-center md:justify-start items-center'>
           <Image
             src='/main-pic.jpeg'
             width={800}
             height={600}
             alt='2024 iftar'
-            className='rounded-md'
+            className='rounded-md border border-2 border-rose-400/30'
           />
         </div>
         <div className='flex-1 flex flex-col justify-center items-center text-center gap-6'>
-          <h1 className='text-rose-300 text-5xl font-serif font-medium'>
+          <h1 className='text-green-950 text-5xl font-serif font-medium'>
             Annual Iftar
           </h1>
-          <h3 className='text-green-800 text-xl font-serif'>
-            Join us to celebrate togetherness!
-          </h3>
-          <p className='text-green-800 text-base font-serif'>March 20, 2025</p>
+          <div className='gap-2 flex flex-col'>
+            <h3 className='text-stone-500 text-xl font-serif'>
+              Join us to celebrate togetherness!
+            </h3>
+            <p className='text-stone-500 text-base font-serif'>
+              March 20, 2025
+            </p>
+          </div>
         </div>
       </section>
 
@@ -142,15 +146,15 @@ export function HKBUIftar() {
         className='w-full px-8 md:px-20 lg:px-40 py-14 bg-zinc-100/50 flex flex-col justify-center items-center gap-14'
       >
         <div className='w-full flex flex-col justify-start items-center gap-6'>
-          <h2 className='text-yellow-600 text-4xl font-serif font-medium leading-tight text-center max-w-lg'>
+          <h2 className='text-green-950 text-4xl font-serif font-medium leading-tight text-center max-w-lg'>
             What is Iftar?
           </h2>
         </div>
         <div className='w-full'>
-          <p className='text-xl text-green-800 leading-relaxed'>
+          <p className='text-xl text-stone-500 leading-relaxed font-serif'>
             Iftar is one of our most festive and memorable annual events where
             we demonstrate the excellence of our Muslim community.
-            <span className='text-red-400'>
+            <span className='text-rose-400'>
               {' '}
               All students and broader community members are invited
             </span>
@@ -161,11 +165,14 @@ export function HKBUIftar() {
             food and dessert.
           </p>
         </div>
-        <div className='w-full aspect-[16/9] bg-zinc-300/50 rounded-md flex items-center justify-center'>
-          <p className='text-center text-sm text-black/70'>
-            Join us for an unforgettable evening of communal iftar to celebrate
-            togetherness.
-          </p>
+        <div className='w-3/4 flex items-center justify-center'>
+          <Image
+            src='/iftar-2.png'
+            alt='Iftar'
+            width={800}
+            height={450}
+            className='rounded-md border border-2 border-rose-400/30'
+          />
         </div>
       </section>
 
@@ -176,7 +183,7 @@ export function HKBUIftar() {
       >
         <div className='w-full flex flex-col justify-start items-center gap-6'>
           <h2 className='text-yellow-600 text-4xl font-serif font-medium text-center max-w-lg'>
-            Event Schedule
+            Schedule
           </h2>
         </div>
 
@@ -194,8 +201,8 @@ export function HKBUIftar() {
         className='w-full px-8 md:px-20 lg:px-40 py-14 bg-[url(/green-bg.png)] bg-cover bg-center flex flex-col justify-center items-center gap-14'
       >
         <div className='w-full flex flex-col justify-start items-center gap-6'>
-          <h2 className='text-rose-400 text-4xl font-serif font-medium text-center max-w-lg'>
-            Event Activities
+          <h2 className='text-green-950 text-4xl font-serif font-medium text-center max-w-lg'>
+            Activities
           </h2>
         </div>
 
@@ -224,7 +231,7 @@ export function HKBUIftar() {
         className='w-full px-8 md:px-20 lg:px-40 py-14 bg-zinc-100/50 flex flex-col justify-center items-center gap-14'
       >
         <div className='w-full flex flex-col justify-start items-center gap-6'>
-          <h2 className='text-yellow-600 text-4xl font-serif font-medium text-center max-w-lg'>
+          <h2 className='text-green-950 text-4xl font-serif font-medium text-center max-w-lg'>
             FAQ
           </h2>
         </div>
@@ -235,29 +242,29 @@ export function HKBUIftar() {
               value='item-1'
               className='border-b border-red-400/30'
             >
-              <AccordionTrigger className='text-green-800 text-xl font-medium'>
+              <AccordionTrigger className='text-green-950 text-xl font-medium'>
                 I am not fasting. Can I attend?
               </AccordionTrigger>
-              <AccordionContent className='text-green-800'>
+              <AccordionContent className='text-green-950'>
                 Yes, this event is open to all HKBU students and staff.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value='item-2'>
-              <AccordionTrigger className='text-green-800 text-xl font-medium'>
+              <AccordionTrigger className='text-green-950 text-xl font-medium'>
                 Is food halal?
               </AccordionTrigger>
-              <AccordionContent className='text-green-800'>
+              <AccordionContent className='text-green-950'>
                 All the food has been ordered from halal certified restaurants
                 and snacks are halal, too.
               </AccordionContent>
             </AccordionItem>
 
             <AccordionItem value='item-3'>
-              <AccordionTrigger className='text-green-800 text-xl font-medium'>
+              <AccordionTrigger className='text-green-950 text-xl font-medium'>
                 I want to volunteer!
               </AccordionTrigger>
-              <AccordionContent className='text-green-800'>
+              <AccordionContent className='text-green-950'>
                 Send an email to Ayazhan.
               </AccordionContent>
             </AccordionItem>
@@ -274,14 +281,14 @@ export function HKBUIftar() {
           <h2 className='text-yellow-600 text-4xl font-serif font-medium leading-tight'>
             Feedback Form
           </h2>
-          <p className='text-green-800'>
+          <p className='text-green-950'>
             Share your thoughts and suggestions with us.
           </p>
         </div>
 
         <div className='flex-1 flex flex-col justify-center items-start gap-10 w-full'>
           <div className='w-full space-y-2'>
-            <label className='text-green-800 text-sm font-medium'>Name</label>
+            <label className='text-green-950 text-sm font-medium'>Name</label>
             <Input
               placeholder='Enter your name'
               className='bg-white border border-black/10'
@@ -289,7 +296,7 @@ export function HKBUIftar() {
           </div>
 
           <div className='w-full space-y-2'>
-            <label className='text-green-800 text-sm font-medium'>Email</label>
+            <label className='text-green-950 text-sm font-medium'>Email</label>
             <Input
               placeholder='Enter your email'
               className='bg-white border border-black/10'
@@ -297,7 +304,7 @@ export function HKBUIftar() {
           </div>
 
           <div className='w-full space-y-2'>
-            <label className='text-green-800 text-sm font-medium'>
+            <label className='text-green-950 text-sm font-medium'>
               Feedback
             </label>
             <Textarea
@@ -313,25 +320,25 @@ export function HKBUIftar() {
       </section>
 
       {/* Footer */}
-      <footer className='w-full p-14 bg-[url(/green-bg.png)] bg-cover flex justify-center items-center'>
+      <footer className='w-full p-14 bg-[url(/green-bg.png)] bg-cover bg-center flex justify-center items-center'>
         <div className='flex flex-col justify-center items-center gap-14'>
-          <div className='text-center text-green-800 text-xl'>
+          <div className='text-center text-green-950 text-xl'>
             Contact
             <br />
             <br />
             <div className='flex gap-4 justify-center'>
-              <a href='#' className='hover:text-green-800 transition-colors'>
+              <a href='#' className='hover:text-green-950 transition-colors'>
                 <span className='sr-only'>WhatsApp</span>
                 📱
               </a>
-              <a href='#' className='hover:text-green-800 transition-colors'>
+              <a href='#' className='hover:text-green-950 transition-colors'>
                 <span className='sr-only'>Email</span>
                 ✉️
               </a>
             </div>
           </div>
           <Separator className='w-full max-w-md' />
-          <div className='text-center text-green-800'>
+          <div className='text-center text-green-950'>
             © 2023 Iftar Event at University. All rights reserved.
           </div>
         </div>
