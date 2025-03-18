98c1a02f9c41d5f01d65c10c23377b2c5139e8cd
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-03-19 03:31:19 +0800

    more faq

--- components/sections/faq.tsx
+++ components/sections/faq.tsx
@@ -1,9 +1,76 @@
+import React from 'react'
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion'
+
+const faqQuestions = [
+  {
+    id: 'item-1',
+    question: 'I am not fasting. Can I attend?',
+    answer: 'Yes, this event is open to all HKBU students and staff.',
+  },
+  {
+    id: 'item-2',
+    question: 'It is my first iftar. What should I know before?',
+    answer: (
+      <>
+        We will have a presentation before dinner where you can learn about
+        Ramadan. If you want to read something beforehand, check out this{' '}
+        <a
+          href='https://www.thekitchn.com/what-you-should-know-before-attending-your-first-iftar-245656'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-green-950/70 hover:text-red-300 transition-colors underline underline-offset-2 underline-thickness-1'
+        >
+          article
+        </a>{' '}
+        and surf the internet.
+      </>
+    ),
+  },
+  {
+    id: 'item-3',
+    question: 'Is food halal?',
+    answer:
+      'All the food has been ordered from halal certified restaurants and snacks are halal, too.',
+  },
+  {
+    id: 'item-4',
+    question: 'What is the dress code?',
+    answer:
+      'Casual clothing is welcome, but please dress modestly to respect the occasion. Avoid tight or revealing outfits, as modesty is important during this religious rite of Ramadan.',
+  },
+  {
+    id: 'item-5',
+    question: 'I want to volunteer!',
+    answer: (
+      <>
+        Send us an{' '}
+        <a
+          href='mailto:kadessovaayazhan@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-green-950/70 hover:text-red-300 transition-colors underline underline-offset-2 underline-thickness-1'
+        >
+          email
+        </a>{' '}
+        or text us on{' '}
+        <a
+          href='https://wa.me/+77778502000'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='hover:text-red-300 transition-transform transform hover:scale-125 underline underline-offset-2 underline-thickness-1'
+        >
+          WhatsApp
+        </a>
+        .
+      </>
+    ),
+  },
+]
 
 export function FAQ() {
   return (
@@ -19,51 +86,20 @@ export function FAQ() {
 
       <div className='w-full space-y-4 py-5'>
         <Accordion type='single' collapsible className='w-full'>
-          <AccordionItem value='item-1' className='border-b border-red-400/30'>
-            <AccordionTrigger className='text-green-950 text-xl font-medium'>
-              I am not fasting. Can I attend?
-            </AccordionTrigger>
-            <AccordionContent className='text-green-950/70'>
-              Yes, this event is open to all HKBU students and staff.
-            </AccordionContent>
-          </AccordionItem>
-
-          <AccordionItem value='item-2' className='border-b border-red-400/30'>
-            <AccordionTrigger className='text-green-950 text-xl font-medium '>
-              Is food halal?
-            </AccordionTrigger>
-            <AccordionContent className='text-green-950/70'>
-              All the food has been ordered from halal certified restaurants and
-              snacks are halal, too.
-            </AccordionContent>
-          </AccordionItem>
-
-          <AccordionItem value='item-3' className='border-b border-red-400/30'>
-            <AccordionTrigger className='text-green-950 text-xl font-medium'>
-              I want to volunteer!
-            </AccordionTrigger>
-            <AccordionContent className='text-green-950/70'>
-              Send us an{' '}
-              <a
-                href='mailto:kadessovaayazhan@gmail.com'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='text-green-950/70 hover:text-red-300 transition-colors underline underline-offset-2 underline-thickness-1'
-              >
-                email
-              </a>{' '}
-              or text us on{' '}
-              <a
-                href='https://wa.me/+77778502000'
-                target='_blank'
-                rel='noopener noreferrer'
-                className='hover:text-red-300 transition-transform transform hover:scale-125 underline underline-offset-2 underline-thickness-1'
-              >
-                WhatsApp
-              </a>
-              .
-            </AccordionContent>
-          </AccordionItem>
+          {faqQuestions.map((item) => (
+            <AccordionItem
+              key={item.id}
+              value={item.id}
+              className='border-b border-red-400/30'
+            >
+              <AccordionTrigger className='text-green-950 text-xl font-medium'>
+                {item.question}
+              </AccordionTrigger>
+              <AccordionContent className='text-green-950/70'>
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
     </section>
