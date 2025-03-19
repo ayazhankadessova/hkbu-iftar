7d9909e9d3be0d95f01fcd2741ea1f27f25d5178
ayazhankadessova <kadessovaayazhan@gmail.com>
2025-03-19 11:52:45 +0800

    add details to faq

--- data/faq-data.tsx
+++ data/faq-data.tsx
@@ -10,7 +10,8 @@ export const faqQuestions: FAQItem[] = [
   {
     id: 'item-1',
     question: 'I am not fasting. Can I attend?',
-    answer: 'Yes, this event is open to all HKBU students and staff. Fasting is not required.',
+    answer:
+      'Yes, this event is open to all HKBU students and staff. Fasting is not required.',
   },
   {
     id: 'item-2',
@@ -57,25 +58,35 @@ export const faqQuestions: FAQItem[] = [
   },
   {
     id: 'item-4',
-    question: 'What is the dress code?',
-    answer:
-      'Casual clothing is welcome, but please dress modestly to respect the occasion. Avoid tight or revealing outfits, as modesty is important during this religious rite of Ramadan.',
-  },
-  {
-    id: 'item-5',
-    question: 'I want to volunteer!',
+    question: 'Will there be a place to pray?',
     answer: (
       <>
-        Send us an{' '}
+        We are planning to use the area behind the stage to provide a prayer
+        space, separate for men and women. Please bring your own prayer mat if
+        you have one. If you prefer a quiet place, you can use the room in {' '}
         <a
-          href='mailto:kadessovaayazhan@gmail.com'
+          href='https://sa.hkbu.edu.hk/en/cla/student-amenities-and-welfare/quiet-rooms.html'
           target='_blank'
           rel='noopener noreferrer'
           className='text-green-950/70 hover:text-red-300 transition-colors underline underline-offset-2 underline-thickness-1'
         >
-          email
-        </a>{' '}
-        or text us on{' '}
+          DLB705
+        </a>{' '} for your prayer.
+      </>
+    ),
+  },
+  {
+    id: 'item-5',
+    question: 'What is the dress code?',
+    answer:
+      'Casual clothing is welcome, but please dress modestly to respect the occasion. Avoid tight or revealing outfits (shorts, tank tops, etc.), as modesty is important during this religious rite of Ramadan.',
+  },
+  {
+    id: 'item-6',
+    question: 'I want to volunteer!',
+    answer: (
+      <>
+        Any help is appreciated! Text us on{' '}
         <a
           href='https://wa.me/+77778502000'
           target='_blank'
@@ -83,8 +94,7 @@ export const faqQuestions: FAQItem[] = [
           className='hover:text-red-300 transition-transform transform hover:scale-125 underline underline-offset-2 underline-thickness-1'
         >
           WhatsApp
-        </a>
-        .
+        </a>, or simply come to AAB 3rd floor at 4.30pm on the day of the event to help set up the tables, food, and decoations.
       </>
     ),
   },
